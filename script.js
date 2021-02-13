b058c0a8e3607ba0a3e0a1da25b2388403f18ee8
Moises <test@tests-MacBook-Pro.local>
2021-02-13 13:20:02 -0800

    [UPDATED]idioma del API de busqueda

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const API_URL    = 'https://api.themoviedb.org/3/discover/movie?api_key=41960dc863fbc6e5d633510d5ca665f9&language=es-MX&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
 const IMG_PATH   = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=41960dc863fbc6e5d633510d5ca665f9&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=41960dc863fbc6e5d633510d5ca665f9&language=es-MX&query="'
 
 const main   = document.getElementById('main');
 const form   = document.getElementById('form');
